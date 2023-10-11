590c808883ad403a96e39d4c3ac954a7f86cdaf8
Goorln <goorln@163.com>
2023-10-11 10:17:58 +0800

    个人中心-修改用户头像

--- src/pagesMember/profile/profile.vue
+++ src/pagesMember/profile/profile.vue
@@ -16,6 +16,37 @@ const getMemberProfile = async () => {
 onLoad(() => {
   getMemberProfile()
 })
+// 修改头像
+const onAvatarChange = () => {
+  // 调用拍照/选择图片
+  uni.chooseMedia({
+    // 文件个数
+    count: 1,
+    // 文件类型
+    mediaType: ['image'],
+    success: (res) => {
+      // console.log(res)
+      // 文件路径
+      const { tempFilePath } = res.tempFiles[0]
+      // 文件上传
+      uni.uploadFile({
+        url: '/member/profile/avatar',
+        name: 'file',
+        filePath: tempFilePath,
+        success: (res) => {
+          console.log(res)
+          if (res.statusCode === 200) {
+            const avatar = JSON.parse(res.data).result.avatar
+            profile.value!.avatar = avatar
+            uni.showToast({ icons: 'success', title: '更新成功' })
+          } else {
+            uni.showToast({ icons: 'error', title: '出现错误' })
+          }
+        },
+      })
+    },
+  })
+}
 </script>
 
 <template>
@@ -27,7 +58,7 @@ onLoad(() => {
     </view>
     <!-- 头像 -->
     <view class="avatar">
-      <view class="avatar-content">
+      <view class="avatar-content" @tap="onAvatarChange">
         <image class="image" :src="profile?.avatar" mode="aspectFill" />
         <text class="text">点击修改头像</text>
       </view>
