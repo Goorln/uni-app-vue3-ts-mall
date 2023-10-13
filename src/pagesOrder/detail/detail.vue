96710c1f8116bc392a3aa90a8382cfc36e7c4fd4
Goorln <goorln@163.com>
2023-10-13 10:57:24 +0800

    订单详情-删除订单功能

--- src/pagesOrder/detail/detail.vue
+++ src/pagesOrder/detail/detail.vue
@@ -3,6 +3,7 @@ import { useGuessList } from '@/composables'
 import { ref } from 'vue'
 import { onReady, onLoad } from '@dcloudio/uni-app'
 import {
+  deleteMemberOrderAPI,
   getMemberOrderByIdAPI,
   getMemberOrderConsignmentByIdAPI,
   getMemberOrderLogisticsByIdAPI,
@@ -130,6 +131,20 @@ const onOrderConfirm = () => {
     },
   })
 }
+
+// 删除订单
+const onOrderDelete = () => {
+  // 二次确认弹窗
+  uni.showModal({
+    content: '是否删除订单？',
+    success: async (res) => {
+      if (res.confirm) {
+        await deleteMemberOrderAPI({ ids: [query.id] })
+        uni.redirectTo({ url: '/pagesOrder/list/list' })
+      }
+    },
+  })
+}
 </script>
 
 <template>
@@ -219,9 +234,9 @@ const onOrderConfirm = () => {
         <view class="item">
           <navigator
             class="navigator"
-            v-for="item in order?.skus"
+            v-for="item in order.skus"
             :key="item.id"
-            :url="`/pages/goods/goods?id=${item.id}`"
+            :url="`/pages/goods/goods?id=${item.spuId}`"
             hover-class="none"
           >
             <image class="cover" :src="item.image"></image>
@@ -238,7 +253,7 @@ const onOrderConfirm = () => {
             </view>
           </navigator>
           <!-- 待评价状态:展示按钮 -->
-          <view class="action" v-if="true">
+          <view class="action" v-if="order.orderState === OrderState.DaiPingJia">
             <view class="button primary">申请售后</view>
             <navigator url="" class="button"> 去评价 </navigator>
           </view>
@@ -247,15 +262,15 @@ const onOrderConfirm = () => {
         <view class="total">
           <view class="row">
             <view class="text">商品总价: </view>
-            <view class="symbol">{{ order?.totalMoney }}</view>
+            <view class="symbol">{{ order?.totalMoney.toFixed(2) }}</view>
           </view>
           <view class="row">
             <view class="text">运费: </view>
-            <view class="symbol">{{ order?.postFee }}</view>
+            <view class="symbol">{{ order?.postFee.toFixed(2) }}</view>
           </view>
           <view class="row">
             <view class="text">应付金额: </view>
-            <view class="symbol primary">{{ order?.payMoney }}</view>
+            <view class="symbol primary">{{ order?.payMoney.toFixed(2) }}</view>
           </view>
         </view>
       </view>
@@ -278,7 +293,7 @@ const onOrderConfirm = () => {
       <view class="toolbar-height" :style="{ paddingBottom: safeAreaInsets?.bottom + 'px' }"></view>
       <view class="toolbar" :style="{ paddingBottom: safeAreaInsets?.bottom + 'px' }">
         <!-- 待付款状态:展示支付按钮 -->
-        <template v-if="true">
+        <template v-if="order.orderState === OrderState.DaiFuKuan">
           <view class="button primary" @tap="onOrderPay"> 去支付 </view>
           <view class="button" @tap="popup?.open?.()"> 取消订单 </view>
         </template>
@@ -289,20 +304,25 @@ const onOrderConfirm = () => {
             :url="`/pagesOrder/create/create?orderId=${query.id}`"
             hover-class="none"
           >
-            再次购买{{ order?.orderState === OrderState.DaiShouHuo }}
+            再次购买
           </navigator>
           <!-- 待收货状态: 展示确认收货 -->
           <view
             class="button primary"
-            v-if="order?.orderState === OrderState.DaiShouHuo"
+            v-if="order.orderState === OrderState.DaiShouHuo"
             @tap="onOrderSend"
+            >确认收货</view
           >
-            确认收货
-          </view>
           <!-- 待评价状态: 展示去评价 -->
-          <view class="button"> 去评价 </view>
+          <view class="button" v-if="order.orderState === OrderState.DaiPingJia"> 去评价 </view>
           <!-- 待评价/已完成/已取消 状态: 展示删除订单 -->
-          <view class="button delete"> 删除订单 </view>
+          <view
+            class="button delete"
+            v-if="order.orderState >= OrderState.DaiPingJia"
+            @tap="onOrderDelete"
+          >
+            删除订单
+          </view>
         </template>
       </view>
     </template>
